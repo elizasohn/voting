36bf6b98487a76a81b996a89ac34e6559873fce2
eliza sohn and veronica stanley-katz <example@example.com>
2019-10-24 10:29:13 -0700

    created if/else statements for age and state along with alerts for those 16 & 17.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 
   var age = parseInt(prompt("How old are you?"));
 
-    if (age < 18) {
+    if (age >= 16 && age < 18) {
+      $(".maybe").show();
+    } else if (age < 16) {
       $(".under").show();
     } else if (age >= 18) {
       $(".over").show();
@@ -11,4 +13,18 @@ $(document).ready(function(){
       $(alert("Sorry, we don't recognize that age. Please try again."));
         window.location.reload();
     }
+
+    $("form#form").submit(function(event){
+      var state = $("select#state").val();
+          if ( state === "1") {
+            $(".maybe").hide();
+            $(".under").show();
+            $(alert("Unfortunately your state doesn't allow you to register but we hope this information is helpful!"))
+          } else {
+            $(".maybe").hide();
+            $(".over").show();
+            $(alert("Congratz! Your state allows you to register under 18!"));
+          }
+            event.preventDefault();
+    });
 });
